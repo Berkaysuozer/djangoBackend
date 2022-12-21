8ff518670e432ef45b8bdc77d37c343e20cb1b82
Berkay Şuözer <berkaysuozer@Berkay-MacBook-Pro.local>
2022-12-22 00:37:28 +0300

    app added

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -61,7 +61,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://127.0.0.1:9000',
     'http://localhost:5173',
-    'http://testbackend-env.eba-7kd2yjex.us-east-1.elasticbeanstalk.com',
+    'http://djangoapplast-env.eba-2u24axpk.us-east-1.elasticbeanstalk.com',    
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -70,7 +70,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:9000',
     'http://localhost:5173',
-    'http://testbackend-env.eba-7kd2yjex.us-east-1.elasticbeanstalk.com',
+    'http://djangoapplast-env.eba-2u24axpk.us-east-1.elasticbeanstalk.com',
 ]
 
 ROOT_URLCONF = 'djangoApp.urls'
